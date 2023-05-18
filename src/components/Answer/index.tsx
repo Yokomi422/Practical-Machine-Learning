fb9c190c1bab6b1d903ab70d77ada5e0ab21517b
Yokomi422 <124508247+Yokomi422@users.noreply.github.com>
2023-05-18 18:21:20 +0900

    いらないファイルの削除

--- src/components/Answer/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { type ReactNode } from "react";
-import Details from "@theme/Details";
-
-/**
- * 解答を書くための、コンポーネント
- */
-export default function Answer({ children }: { children: ReactNode }) {
-  return <Details summary={<summary>解答</summary>}>{children}</Details>;
-}
