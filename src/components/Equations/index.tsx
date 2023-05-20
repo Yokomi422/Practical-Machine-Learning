5909f305e9cae9ca786d5aa122731a8050af346a
Yokomi422 <124508247+Yokomi422@users.noreply.github.com>
2023-05-20 14:22:20 +0900

    Latexが反映されるようにした

---
+++ src/components/Equations/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'katex/dist/katex.min.css';
+import { BlockMath } from 'react-katex';
+
+export default function MyComponent() {
+  return (
+    <div>
+        
+    </div>
+  );
+}
