12b09193e9645633eba2b80c49340ec63ea789bb
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-05-22 14:52:25 +0900

    Remove document URL from footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,15 +92,6 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
             title: 'Community',
             items: [
               {
