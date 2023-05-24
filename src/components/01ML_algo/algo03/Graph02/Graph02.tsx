a4e21fdbc3aeb9d37cae8e299e5e81ad15a16cb7
Yokomi422 <124508247+Yokomi422@users.noreply.github.com>
2023-05-24 17:53:08 +0900

    木構造の学習の箇所の木構造とグラフを追加した

--- src/components/01ML_algo/algo03/Graph02/Graph02.tsx
+++ src/components/01ML_algo/algo03/Graph02/Graph02.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import Graph0201 from "./Graph0201/Graph0201";
+import Graph0202 from "./Graph0202/Graph0202";
+import Graph0203 from "./Graph0203/Graph0203"
 import Tree03 from "../Tree03/Tree.03";
-import Graph0202 from "./Graph0201/Graph0201";
+import Tree04 from "../Tree04/tree04";
+import Tree05 from "../Tree05/tree05";
 
 export default function Graph02() {
   return (
     <div>
       <Tree03 />
-      <Graph0202 />
+      <Graph0201 />
+      <Tree04 />
+      <Graph0202/>
+      <Tree05 />
+      <Graph0203 />
     </div>
   );
 }
