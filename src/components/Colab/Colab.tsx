addbb89cba567c1167d1b4ddadd5315563c03df6
Yokomi422 <124508247+Yokomi422@users.noreply.github.com>
2023-06-04 23:09:56 +0900

    colabで見れるようにした

--- src/components/Colab/Colab.tsx
+++ src/components/Colab/Colab.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-export default function Colab(url: string) {
+interface Props {
+    url: string;
+}
+
+export default function Colab( { url }: Props) {
     return (
         <a href={url} target="_blank">
             <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
