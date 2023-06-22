355b2958f4887d4ed948b49c81c2a5d19399ba7c
Yokomi422 <124508247+Yokomi422@users.noreply.github.com>
2023-06-22 20:44:54 +0900

    ESLintとPrettierを設定してみた

--- src/components/Answer/Answer.tsx
+++ src/components/Answer/Answer.tsx
@@ -5,5 +5,5 @@ import Details from "@theme/Details";
  * 解答を書くための、コンポーネント
  */
 export default function Answer({ children }: { children: ReactNode }) {
-  return <Details summary={<summary>解答</summary>}>{children}</Details>;
+    return <Details summary={<summary>解答</summary>}>{children}</Details>;
 }
